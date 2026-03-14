be82aafd6ccd570caa7c8e9402e772acd7c76871
Imron Mirzarasulov <imronimronov32@gmail.com>
2026-03-14 16:13:13 +0500

    init

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Sean Allen on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
